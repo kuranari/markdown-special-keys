3ee42ed91dedbe7c111e833766708877fcae074a
Tomohisa Kuranari <tomohisa.kuranari@gmail.com>
2023-02-23 21:14:08 +0900

    Modify code comment

--- markdown-special-keys.el
+++ markdown-special-keys.el
@@ -35,8 +35,6 @@
    ((markdown-on-heading-p)(markdown-beginning-of-line--heading))
    (t (mwim-beginning-of-code-or-line))))
 
-;; コードの良し悪しの判断ができない。mwimの内部構造を理解すると、もっと簡易な記述ができるのかもしれない
-;; [nits] 行頭からコード行頭の間の空白文字の領域で関数を呼び出しした場合に、行頭ではなく、コード行頭にカーソルが移動してしまう挙動がやや気になる。
 (defun markdown-beginning-of-line--list ()
   (cond
    ;; 1. リスト本文の先頭の場合は、bulletの前に移動
@@ -54,11 +52,11 @@
 
 (defun markdown-beginning-of-line--heading ()
   (cond
-   ;; 1. 見出し本文の先頭の場合は、行頭に移動
+   ;; 1. 見出し文字列の先頭の場合は、行頭に移動
    ;; 見出しの記号がinvisibleになっている可能性があるので markdown-move-heading-common を使って移動する
    ((looking-back markdown-regex-header-atx-asynmetric)
     (markdown-move-heading-common #'beginning-of-line nil 'adjust))
-   ;; 2. それ以外の場合は、リスト本文の先頭に移動
+   ;; 2. それ以外の場合は、見出し文字列の先頭に移動
    (t (progn
         (beginning-of-line)
         (re-search-forward markdown-regex-header-atx-asynmetric)))))
@@ -66,14 +64,13 @@
 ;; 本当は 下記のような設定を加えて mwim のオプションに追加したい
 ;; (add-to-list 'mwim-beginning-of-line-function '(markdown-mode . markdown-beginning-of-line))
 ;; しかし list 以外の要素で Ctrl-a が使えなくなってしまう問題が残るので諦めた(調査時間の問題)
-;; 代わりにキーバインドを直接割り当てている
 (evil-define-key 'hybrid markdown-mode-map (kbd "C-a") 'markdown-beginning-of-line)
 
 ;; カーソルが非表示の文字列の上にある場合には、1文字前進させる。
 ;; evil-modeでinsert-modeからnormal-modeに戻る時に、カーソルが一文字後退する(normal-modeでiを押下し、直後にEscを押下するとその挙動がわかる)
 ;; markdown-hide-markupがtの場合、テキストプロパティ(display "")により非表示の文字にカーソルが移動することがあるためこれを防ぐ
 ;;
-;; TODO: move-beginning-of-line も同じような処理をしているかも
+;; 要調査: move-beginning-of-line も同じような処理をしているかもいレナい
 (defun markdown-adjust-hidden-markup ()
   ;; (point) は normal-mode に戻って1文字後退した地点になる
   (when (and markdown-hide-markup
